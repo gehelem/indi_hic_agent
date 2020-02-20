6492c9d7aeccfa2d8a04969dea5b636f3d7ea646
gehelem <gilles@joag.fr>
2020-02-20 12:02:28 +0100

    Add files

---
+++ indi_hic_agent.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Agent">
+        <device label="HIC Agent" mdpd="true">
+                <driver name="HIC Agent">indi_hic_agent</driver>
+                <version>@HICAGENT_VERSION_MAJOR@.@HICAGENT_VERSION_MINOR@</version>
+	</device>
+</devGroup>
+</driversList>
